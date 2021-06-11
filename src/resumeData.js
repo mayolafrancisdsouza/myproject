633fa45074a455a92263208b3bf167333ba22b6d
mayolafrancisdsouza <85723113+mayolafrancisdsouza@users.noreply.github.com>
2021-06-12 03:21:51 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,53 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/mayolafrancisdsouza",
+    "name": "Mayola Dsouza",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/mayolafrancisdsouza",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Looking for a challenging role in a reputable organization to utilize my technical, database, and management skills for the growth of the organization as well as to enhance my knowledge about new and emerging trends in the IT sector.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mayolafrancisdsouza",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mayolafrancisdsouza",
+          "className":"fa fa-github"
+        },
+        {
+         
+    
+        }
+      ],
+    "aboutme":"I am currently a final year student at St Agnes College(Autonomous) and pursuing my BCA from here. I am a Full Stack Web Developer, currently learning new web technology. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Bhatkal,India",
+    "education":[
+      {
+        "UniversityName":"ST AGNES COLLEGE",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"1st place in photography event"
+      },
+      
+    ],
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+     
+    ],
+    
+  }
+  
+  export default resumeData
